fe342ed4a88a6e89c64a2f073add13e871d40f48
Jun Oh <jsohpennstater@gmail.com>
2016-10-17 17:57:09 -0400

    updated hound violations for empty spaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   resources :questionnaires
   root 'questionnaires#index'
-
-
   devise_for :users
   resources :users
-  
   post "/archive_user", to: "users#archive_user"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
