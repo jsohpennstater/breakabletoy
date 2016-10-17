a1b7c0edff2fc7d5156a30ff4d5bca8c673672ce
Jun Oh <jsohpennstater@gmail.com>
2016-10-17 12:49:25 -0400

    set up devise and created questionnaires MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :questionnaires
+  root 'questionnaires#index'
+  devise_for :users
+  post "/archive_user", to: "users#archive_user"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
