cb27ea555e647c4de0e0da789b51a8c3a8eab446
Jun Oh <jsohpennstater@gmail.com>
2016-10-19 23:55:52 -0400

    added models

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ApplicationRecord
   validates :password, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :assignments
+  has_many :questionnaires, :through => :assignments
 end
