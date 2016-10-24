4559dd5da5c4916178f9a2a18832c68d69662c96
Jun Oh <jsohpennstater@gmail.com>
2016-10-24 18:02:02 -0400

    remove all es6 syntax from js files

--- app/assets/javascripts/answer.js
+++ app/assets/javascripts/answer.js
@@ -4,16 +4,16 @@ $('.ScaleOne').on('click', function(event) {
     event.preventDefault();
     var path = this.parentElement.action
     var request = $.ajax({
-      url: `${path}`,
+      url: path,
       method: "POST",
     });
     var new_path = this.parentElement.action.slice(0,-9);
     var request = $.ajax({
-      url: `${new_path}.json`
+      url: new_path + ".json"
     });
     request.done(function(data) {
-      var Scale = `Scale_${data.question_id}`;
-      var Answer = `Not at all`;
+      var Scale = "Scale_" + data.question_id;
+      var Answer = "Not at all";
       document.getElementById(Scale).innerHTML = Answer;
     });
   });
@@ -22,16 +22,16 @@ $('.ScaleTwo').on('click', function(event) {
     event.preventDefault();
     var path = this.parentElement.action;
     var request = $.ajax({
-      url: `${path}`,
+      url: path,
       method: "POST",
     });
     var new_path = this.parentElement.action.slice(0,-9);
     var request = $.ajax({
-      url: `${new_path}.json`
+      url: new_path + ".json"
     });
     request.done(function(data) {
-      var Scale = `Scale_${data.question_id}`;
-      var Answer = `A little bit`;
+      var Scale = "Scale_" + data.question_id;
+      var Answer = "A little bit";
       document.getElementById(Scale).innerHTML = Answer;
     });
   });
@@ -40,16 +40,16 @@ $('.ScaleThree').on('click', function(event) {
     event.preventDefault();
     var path = this.parentElement.action;
     var request = $.ajax({
-      url: `${path}`,
+      url: path,
       method: "POST",
     });
     var new_path = this.parentElement.action.slice(0,-11);
     var request = $.ajax({
-      url: `${new_path}.json`
+      url: new_path + ".json"
     });
     request.done(function(data) {
-      var Scale = `Scale_${data.question_id}`;
-      var Answer = `Somewhat`;
+      var Scale = "Scale_" + data.question_id;
+      var Answer = "Somewhat";
       document.getElementById(Scale).innerHTML = Answer;
     });
   });
@@ -58,16 +58,16 @@ $('.ScaleFour').on('click', function(event) {
     event.preventDefault();
     var path = this.parentElement.action;
     var request = $.ajax({
-      url: `${path}`,
+      url: path,
       method: "POST",
     });
     var new_path = this.parentElement.action.slice(0,-10);
     var request = $.ajax({
-      url: `${new_path}.json`
+      url: new_path + ".json"
     });
     request.done(function(data) {
-      var Scale = `Scale_${data.question_id}`;
-      var Answer = `Quite a bit`;
+      var Scale = "Scale_" + data.question_id;
+      var Answer = "Quite a bit";
       document.getElementById(Scale).innerHTML = Answer;
     });
   });
@@ -76,16 +76,16 @@ $('.ScaleFive').on('click', function(event) {
     event.preventDefault();
     var path = this.parentElement.action;
     var request = $.ajax({
-      url: `${path}`,
+      url: path,
       method: "POST",
     });
     var new_path = this.parentElement.action.slice(0,-10);
     var request = $.ajax({
-      url: `${new_path}.json`
+      url: new_path + ".json"
     });
     request.done(function(data) {
-      var Scale = `Scale_${data.question_id}`;
-      var Answer = `Very Much`;
+      var Scale = "Scale_" + data.question_id;
+      var Answer = "Very Much";
       document.getElementById(Scale).innerHTML = Answer;
     });
   });
@@ -94,12 +94,12 @@ $('.Submit').on('click', function(event) {
       event.preventDefault();
       var path = this.parentElement.action
       var request = $.ajax({
-        url: `${path}`,
+        url: path,
         method: "POST",
       });
       request.done(function(data) {
-        var status = `status_completion`;
-        var completion = `Status: Complete`;
+        var status = "status_completion";
+        var completion = "Status: Complete";
         document.getElementById(status).innerHTML = completion;
       });
     });
