ff54906db2ede6cae57ce0f8adbfb25d0aff38b3
Jun Oh <jsohpennstater@gmail.com>
2016-10-24 17:44:06 -0400

    remove es6 syntax

--- app/assets/javascripts/answer.js
+++ app/assets/javascripts/answer.js
@@ -2,104 +2,104 @@ $(document).ready(function(){
 
 $('.ScaleOne').on('click', function(event) {
     event.preventDefault();
-    let path = this.parentElement.action
+    var path = this.parentElement.action
     var request = $.ajax({
       url: `${path}`,
       method: "POST",
     });
-    let new_path = this.parentElement.action.slice(0,-9);
+    var new_path = this.parentElement.action.slice(0,-9);
     var request = $.ajax({
       url: `${new_path}.json`
     });
     request.done(function(data) {
-      let Scale = `Scale_${data.question_id}`;
-      let Answer = `Not at all`;
+      var Scale = `Scale_${data.question_id}`;
+      var Answer = `Not at all`;
       document.getElementById(Scale).innerHTML = Answer;
     });
   });
 
 $('.ScaleTwo').on('click', function(event) {
     event.preventDefault();
-    let path = this.parentElement.action;
+    var path = this.parentElement.action;
     var request = $.ajax({
       url: `${path}`,
       method: "POST",
     });
-    let new_path = this.parentElement.action.slice(0,-9);
+    var new_path = this.parentElement.action.slice(0,-9);
     var request = $.ajax({
       url: `${new_path}.json`
     });
     request.done(function(data) {
-      let Scale = `Scale_${data.question_id}`;
-      let Answer = `A little bit`;
+      var Scale = `Scale_${data.question_id}`;
+      var Answer = `A little bit`;
       document.getElementById(Scale).innerHTML = Answer;
     });
   });
 
 $('.ScaleThree').on('click', function(event) {
     event.preventDefault();
-    let path = this.parentElement.action;
+    var path = this.parentElement.action;
     var request = $.ajax({
       url: `${path}`,
       method: "POST",
     });
-    let new_path = this.parentElement.action.slice(0,-11);
+    var new_path = this.parentElement.action.slice(0,-11);
     var request = $.ajax({
       url: `${new_path}.json`
     });
     request.done(function(data) {
-      let Scale = `Scale_${data.question_id}`;
-      let Answer = `Somewhat`;
+      var Scale = `Scale_${data.question_id}`;
+      var Answer = `Somewhat`;
       document.getElementById(Scale).innerHTML = Answer;
     });
   });
 
 $('.ScaleFour').on('click', function(event) {
     event.preventDefault();
-    let path = this.parentElement.action;
+    var path = this.parentElement.action;
     var request = $.ajax({
       url: `${path}`,
       method: "POST",
     });
-    let new_path = this.parentElement.action.slice(0,-10);
+    var new_path = this.parentElement.action.slice(0,-10);
     var request = $.ajax({
       url: `${new_path}.json`
     });
     request.done(function(data) {
-      let Scale = `Scale_${data.question_id}`;
-      let Answer = `Quite a bit`;
+      var Scale = `Scale_${data.question_id}`;
+      var Answer = `Quite a bit`;
       document.getElementById(Scale).innerHTML = Answer;
     });
   });
 
 $('.ScaleFive').on('click', function(event) {
     event.preventDefault();
-    let path = this.parentElement.action;
+    var path = this.parentElement.action;
     var request = $.ajax({
       url: `${path}`,
       method: "POST",
     });
-    let new_path = this.parentElement.action.slice(0,-10);
+    var new_path = this.parentElement.action.slice(0,-10);
     var request = $.ajax({
       url: `${new_path}.json`
     });
     request.done(function(data) {
-      let Scale = `Scale_${data.question_id}`;
-      let Answer = `Very Much`;
+      var Scale = `Scale_${data.question_id}`;
+      var Answer = `Very Much`;
       document.getElementById(Scale).innerHTML = Answer;
     });
   });
 
 $('.Submit').on('click', function(event) {
       event.preventDefault();
-      let path = this.parentElement.action
+      var path = this.parentElement.action
       var request = $.ajax({
         url: `${path}`,
         method: "POST",
       });
       request.done(function(data) {
-        let status = `status_completion`;
-        let completion = `Status: Complete`;
+        var status = `status_completion`;
+        var completion = `Status: Complete`;
         document.getElementById(status).innerHTML = completion;
       });
     });
