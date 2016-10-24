5b186527737d8e0ad98d6fff83eff7069b45317a
Jun Oh <jsohpennstater@gmail.com>
2016-10-24 17:36:31 -0400

    remove debugger:

--- app/assets/javascripts/answer.js
+++ app/assets/javascripts/answer.js
@@ -100,7 +100,6 @@ $('.Submit').on('click', function(event) {
       request.done(function(data) {
         let status = `status_completion`;
         let completion = `Status: Complete`;
-        debugger;
         document.getElementById(status).innerHTML = completion;
       });
     });
