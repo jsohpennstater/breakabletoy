b8eb3e91ada049771431ac31c3aaafd915cb3e6f
Jun Oh <jsohpennstater@gmail.com>
2016-10-25 18:18:03 -0400

    created app div in questionnaire show page

--- react/src/components/App.js
+++ react/src/components/App.js
@@ -1,51 +1,147 @@
 import React, {Component} from 'react';
-import Question from './Question'
+// import Question from './Question'
+// 
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       chosenAnswerId: null
+//     };
+//   this.handleQuestionClick = this.handleQuestionClick.bind(this)
+//   };
+//
+//   handleQuestionClick(id) {
+//     this.setState({ chosenAnswerId: id});
+//   }
+//
+//   render() {
+//     let response = "";
+//     let question = this.props.data.question.body;
+//     let answers = this.props.data.answers.map(answer => {
+//       let onQuestionClick = () =>
+//        this.handleQuestionClick(answer.id);
+//         if (this.state.chosenAnswerId === answer.id) {
+//           if (answer.correct) {
+//              response = "Correct"
+//           } else {
+//              response = "Incorrect"
+//             }
+//         }
+//         return (
+//           <Answer
+//             key={answer.id}
+//             id={answer.question_id}
+//             body={answer.body}
+//             onQuestionClick={onQuestionClick}
+//           />
+//         );
+//       });
+//
+//     return (
+//       <div className="react-quiz">
+//         {question}
+//         {answers}
+//         {response}
+//       </div>
+//     )
+//   };
+// };
+
+
+export default App;
+
+
+import React, { Component } from 'react';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      chosenAnswerId: null
-    };
-  this.handleQuestionClick = this.handleQuestionClick.bind(this)
-  };
-
-  handleQuestionClick(id) {
-    this.setState({ chosenAnswerId: id});
+      current: "",
+    }
+    this.handleButtonNext = this.handleButtonNext.bind(this);
+    this.handleButtonBefore = this.handleButtonBefore.bind(this);
+    this.handleButtonTrue = this.handleButtonTrue.bind(this);
   }
 
+  handleButtonNext() {
+    if (this.state.current[0].criteria === "A") {
+        $.ajax({
+          url: `/questionnaires/${this.props.id}.json`,
+          contentType: 'application/json'
+        })
+        .done(data => {
+          this.setState({ current: data.qtwo });
+        });
+      } else if (this.state.current[0].criteria === "B") {
+        $.ajax({
+          url: `/questionnaires/${this.props.id}.json`,
+          contentType: 'application/json'
+        })
+        .done(data => {
+          this.setState({ current: data.qthree });
+        });
+      };
+  }
+
+  handleButtonBefore() {
+    if (this.state.current[0].criteria === "B") {
+        $.ajax({
+          url: `/questionnaires/${this.props.id}.json`,
+          contentType: 'application/json'
+        })
+        .done(data => {
+          this.setState({ current: data.qone });
+        });
+      } else if (this.state.current[0].criteria === "C") {
+        $.ajax({
+          url: `/questionnaires/${this.props.id}.json`,
+          contentType: 'application/json'
+        })
+        .done(data => {
+          this.setState({ current: data.qtwo });
+        });
+      };
+  }
+
+  handleButtonTrue() {
+    document.getElementsByClassName.innerHTML = 'False';
+  }
+
+
+
+  componentDidMount() {
+    $.ajax({
+      url: `/questionnaires/${this.props.id}.json`,
+      contentType: 'application/json'
+    })
+    .done(data => {
+      this.setState({ current: data.qone });
+    });
+  }
+
+
+
   render() {
-    let response = "";
-    let question = this.props.data.question.body;
-    let answers = this.props.data.answers.map(answer => {
-      let onQuestionClick = () =>
-       this.handleQuestionClick(answer.id);
-        if (this.state.chosenAnswerId === answer.id) {
-          if (answer.correct) {
-             response = "Correct"
-          } else {
-             response = "Incorrect"
-            }
-        }
-        return (
-          <Answer
-            key={answer.id}
-            id={answer.question_id}
-            body={answer.body}
-            onQuestionClick={onQuestionClick}
-          />
-        );
+    let questionSet = "";
+    let current= "";
+    if (this.state.current.length !== 0) {
+      questionSet = this.state.current.map(question => {
+        return(
+          <div className="row callout" key={question.id}>
+            <p className="small-2 columns">{question.content}</p>
+            <button className="True" onClick={this.handleButtonTrue}>True</button>
+          </div>
+        )
       });
-
-    return (
-      <div className="react-quiz">
-        {question}
-        {answers}
-        {response}
+    }
+    return(
+      <div className="questionnaire-walkthrough">
+        {questionSet}
+        <button className="previous button" onClick={this.handleButtonBefore}>Previous Question Set</button>
+        <button className="next button" onClick={this.handleButtonNext}>Next Question Set</button>
       </div>
-    )
-  };
-};
-
-
+    );
+  }
+}
 export default App;
--- react/src/main.js
+++ react/src/main.js
@@ -5,7 +5,116 @@ import App from './components/App';
 
 $(function() {
   ReactDOM.render(
-    <App data={data}/>,
+    <App id={window.location.href.split("/")[4]}/>,
     document.getElementById('app')
   );
 });
+
+
+
+// 
+// $('.ScaleOne').on('click', function(event) {
+//     event.preventDefault();
+//     var path = this.parentElement.action
+//     var request = $.ajax({
+//       url: path,
+//       method: "POST",
+//     });
+//     var new_path = this.parentElement.action.slice(0,-9);
+//     var request = $.ajax({
+//       url: new_path + ".json"
+//     });
+//     request.done(function(data) {
+//       var Scale = "Scale_" + data.question_id;
+//       var Answer = "Not at all";
+//       document.getElementById(Scale).innerHTML = Answer;
+//     });
+//   });
+//
+// $('.ScaleTwo').on('click', function(event) {
+//     event.preventDefault();
+//     var path = this.parentElement.action;
+//     var request = $.ajax({
+//       url: path,
+//       method: "POST",
+//     });
+//     var new_path = this.parentElement.action.slice(0,-9);
+//     var request = $.ajax({
+//       url: new_path + ".json"
+//     });
+//     request.done(function(data) {
+//       var Scale = "Scale_" + data.question_id;
+//       var Answer = "A little bit";
+//       document.getElementById(Scale).innerHTML = Answer;
+//     });
+//   });
+//
+// $('.ScaleThree').on('click', function(event) {
+//     event.preventDefault();
+//     var path = this.parentElement.action;
+//     var request = $.ajax({
+//       url: path,
+//       method: "POST",
+//     });
+//     var new_path = this.parentElement.action.slice(0,-11);
+//     var request = $.ajax({
+//       url: new_path + ".json"
+//     });
+//     request.done(function(data) {
+//       var Scale = "Scale_" + data.question_id;
+//       var Answer = "Somewhat";
+//       document.getElementById(Scale).innerHTML = Answer;
+//     });
+//   });
+//
+// $('.ScaleFour').on('click', function(event) {
+//     event.preventDefault();
+//     var path = this.parentElement.action;
+//     var request = $.ajax({
+//       url: path,
+//       method: "POST",
+//     });
+//     var new_path = this.parentElement.action.slice(0,-10);
+//     var request = $.ajax({
+//       url: new_path + ".json"
+//     });
+//     request.done(function(data) {
+//       var Scale = "Scale_" + data.question_id;
+//       var Answer = "Quite a bit";
+//       document.getElementById(Scale).innerHTML = Answer;
+//     });
+//   });
+//
+// $('.ScaleFive').on('click', function(event) {
+//     event.preventDefault();
+//     var path = this.parentElement.action;
+//     var request = $.ajax({
+//       url: path,
+//       method: "POST",
+//     });
+//     var new_path = this.parentElement.action.slice(0,-10);
+//     var request = $.ajax({
+//       url: new_path + ".json"
+//     });
+//     request.done(function(data) {
+//       var Scale = "Scale_" + data.question_id;
+//       var Answer = "Very Much";
+//       document.getElementById(Scale).innerHTML = Answer;
+//     });
+//   });
+//
+// $('.Submit').on('click', function(event) {
+//       event.preventDefault();
+//       var path = this.parentElement.action
+//       var request = $.ajax({
+//         url: path,
+//         method: "POST",
+//       });
+//       request.done(function(data) {
+//         var status = "status_completion";
+//         var completion = "Status: Complete";
+//         document.getElementById(status).innerHTML = completion;
+//       });
+//     });
+//
+// });
