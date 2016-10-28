c84c4936a9bbf65a3ce74daa655a781e2093a97b
Jun Oh <jsohpennstater@gmail.com>
2016-10-28 04:19:17 -0400

    changed buttons for users

--- react/src/components/Question.js
+++ react/src/components/Question.js
@@ -4,24 +4,26 @@ let Question = props => {
   return (
     <div className="large-12 columns userquestion-display" key={props.id}>
       <h1 className="row question-content">{props.content}</h1>
-      <div className="row">
+      <div className="row scale">
         <p onClick={props.oneQuestionClick}>
-          <button className="ScaleOne">Not at all</button>
+          <button className="large-1 columns ScaleOne">Not at all</button>
         </p>
         <p onClick={props.twoQuestionClick}>
-          <button className="ScaleTwo">A little bit</button>
+          <button className="large-1 columns ScaleTwo">A little bit</button>
         </p>
         <p onClick={props.threeQuestionClick}>
-          <button className="ScaleThree">Somewhat</button>
+          <button className="large-1 columns ScaleThree">Somewhat</button>
         </p>
         <p onClick={props.fourQuestionClick}>
-          <button className="ScaleFour">Quite a bit</button>
+          <button className="large-1 columns ScaleFour">Quite a bit</button>
         </p>
         <p onClick={props.fiveQuestionClick}>
-          <button className="ScaleFive">Very Much</button>
+          <button className="large-1 columns ScaleFive">Very Much</button>
         </p>
       </div>
+      <div className="responsetext">
       <p id={`response_${props.questionId}`}>""</p>
+    </div>
     </div>
   );
 };
